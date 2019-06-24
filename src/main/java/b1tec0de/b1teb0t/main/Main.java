5206cc344eaee3124a42b5088cfde51e2cf4a9e3
Kaufisch <kaufischlp@gmail.com>
2019-06-24 22:51:20 +0200

    Update ConfigManager
    Add key's for Database
    Add error messages when config.json is not found and when using default values in config.json

--- src/main/java/b1tec0de/b1teb0t/main/Main.java
+++ src/main/java/b1tec0de/b1teb0t/main/Main.java
@@ -5,12 +5,15 @@ import b1tec0de.b1teb0t.utils.ConfigManager;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
 
-    static JDA jda;
+    private static JDA jda;
+    private static Logger logger = LoggerFactory.getLogger(ConfigManager.class);
 
     public static void main(String[] args) {
         System.out.println("  ____  _ _        ____ ___      _");
@@ -20,12 +23,24 @@ public class Main {
         System.out.println(" |____/|_|\\__\\___|\\____\\___/ \\__,_|\\___|");
         System.out.println();
         ConfigManager cm = new ConfigManager();
+        cm.getAuthToken();
         try {
-            jda = new JDABuilder(AccountType.BOT)
-                    .setToken(cm.getAuthToken())
-                    .setAutoReconnect(true)
-                    .addEventListener(new CommandManager())
-                    .build();
+            String token = cm.getAuthToken();
+            if(token.equals("not found")) {
+                logger.error("config.json not found!\n");
+                System.exit(0);
+            }
+            else if(token.equals("default values")) {
+                logger.error("Please make sure that you fill config.json with values!");
+                System.exit(0);
+            }
+            else {
+                jda = new JDABuilder(AccountType.BOT)
+                        .setToken(cm.getAuthToken())
+                        .setAutoReconnect(true)
+                        .addEventListener(new CommandManager())
+                        .build();
+            }
         } catch (LoginException e) {
             e.printStackTrace();
         }
--- src/main/java/b1tec0de/b1teb0t/utils/ConfigManager.java
+++ src/main/java/b1tec0de/b1teb0t/utils/ConfigManager.java
@@ -1,10 +1,14 @@
 package b1tec0de.b1teb0t.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.HashMap;
 
 /**
  * Reads the Bot Auth Token from config.json and returns it.
@@ -15,31 +19,36 @@ import java.io.FileReader;
 public class ConfigManager {
 
     private Gson gson;
+    private BufferedReader br;
+
 
     public ConfigManager() {
         gson = new Gson();
     }
 
-    public static class Config {
-
-        String TOKEN;
-
-        String getToken() {
-            return TOKEN;
-        }
-
-    }
-
     public String getAuthToken() {
         try {
-            BufferedReader br = new BufferedReader(new FileReader("./config.json"));
-            Config config = gson.fromJson(br, Config.class);
-            return config.getToken();
-
+            br = new BufferedReader(new FileReader("./config.json"));
+            JsonObject config = gson.fromJson(br, JsonObject.class);
+            String TOKEN = config.getAsJsonObject("Discord").get("TOKEN").getAsString();
+            if(TOKEN.equals("YourBotToken")) {
+                return "default values";
+            }
+            else {
+                return TOKEN;
+            }
         } catch (FileNotFoundException e) {
-            System.out.println("[ERROR] config.json not found!");
-            return null;
+            return "not found";
         }
+    }
+
+    public HashMap getDatabaseCreds() {
+        JsonObject config = gson.fromJson(br, JsonObject.class);
+        HashMap<String, String> creds = new HashMap<>();
+        creds.put("user", config.getAsJsonObject("Database").get("user").getAsString());
+        creds.put("password", config.getAsJsonObject("Database").get("password").getAsString());
+        creds.put("database", config.getAsJsonObject("Database").get("database").getAsString());
+        return creds;
     }
 
 }
