909e07d788878676880c8d05fbb90cd0f055b74a
Kaufisch <kaufischlp@gmail.com>
2019-06-26 21:26:02 +0200

    Add WarnSystem

---
+++ src/main/java/b1tec0de/b1teb0t/events/Unban.java
@@ -0,0 +1,15 @@
+package b1tec0de.b1teb0t.events;
+
+import b1tec0de.b1teb0t.utils.Database;
+import net.dv8tion.jda.core.events.guild.GuildUnbanEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class Unban extends ListenerAdapter {
+
+    @Override
+    public void onGuildUnban(GuildUnbanEvent event) {
+        Database db = new Database();
+        db.deleteUser(event.getGuild().getId(), event.getUser().getId());
+    }
+
+}
