56d203f9eb7cee49ad4131ea37d94afadf39e6ad
JavaBasti <javabasti123@gmail.com>
2019-06-27 14:15:22 +0200

    ActionLog finished

--- src/main/java/b1tec0de/b1teb0t/events/GuildJoin.java
+++ src/main/java/b1tec0de/b1teb0t/events/GuildJoin.java
@@ -3,6 +3,9 @@ package b1tec0de.b1teb0t.events;
 import b1tec0de.b1teb0t.main.Main;
 import b1tec0de.b1teb0t.utils.Database;
 import b1tec0de.b1teb0t.utils.objects.GuildConfig;
+import net.dv8tion.jda.core.entities.Channel;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -24,5 +27,10 @@ public class GuildJoin extends ListenerAdapter {
         guildConfig.setGuildId(e.getGuild().getId());
         guildConfig.setPrefix("!");
         Main.guildConfigs.add(guildConfig);
+
+        // Create ActionLog Channel on Join
+
+        // Guild guild = e.getGuild();
+        // Channel ch = guild.getController().createTextChannel("ActionLog").complete();
     }
 }
--- src/main/java/b1tec0de/b1teb0t/main/Main.java
+++ src/main/java/b1tec0de/b1teb0t/main/Main.java
@@ -1,9 +1,6 @@
 package b1tec0de.b1teb0t.main;
 
-import b1tec0de.b1teb0t.events.BotStarted;
-import b1tec0de.b1teb0t.events.CommandManager;
-import b1tec0de.b1teb0t.events.GuildJoin;
-import b1tec0de.b1teb0t.events.Unban;
+import b1tec0de.b1teb0t.events.*;
 import b1tec0de.b1teb0t.utils.ConfigManager;
 import b1tec0de.b1teb0t.utils.objects.GuildConfig;
 import net.dv8tion.jda.core.AccountType;
@@ -48,6 +45,7 @@ public class Main {
                         .addEventListener(new BotStarted())
                         .addEventListener(new CommandManager())
                         .addEventListener(new GuildJoin())
+                        .addEventListener(new ActionLogEvent())
                         .addEventListener(new Unban())
                         .build();
             }
